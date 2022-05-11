4f80fe62e55b2a732432ba3229d291462b9c79cd
matteobarbon <105351223+matteobarbon@users.noreply.github.com>
2022-05-11 14:18:36 +0200

    Adding import module command

--- merakihafa/run.ps1
+++ merakihafa/run.ps1
@@ -42,6 +42,7 @@
 #
 #--------------------------------------------------------------------------
 param($Timer)
+import-module ‘D:\Home\site\wwwroot\nvadrg\Modules\azurerm.6.13.2\AzureRM.psm1’
 Write-Output -InputObject "HA NVA timer trigger function executed at:$(Get-Date)"
 
 #--------------------------------------------------------------------------
